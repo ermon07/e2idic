8545e3b5db20dd7d4bea7cee3ec55688821d58f3
ermon07 <ermon1995@gmail.com>
2026-03-31 00:50:17 +0800

    service-worker

--- script.js
+++ script.js
@@ -113,32 +113,3 @@ function searchWord() {
   }
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
-}
-
-let deferredPrompt;
-const installButton = document.getElementById('install-button'); // Your custom button
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  installButton.style.display = 'block';
-});
-
-installButton.addEventListener('click', (e) => {
-  // Hide our user interface that shows our A2HS button
-  installButton.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    }
-    deferredPrompt = null;
-  });
-});
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,34 @@
-self.addEventListener('fetch', function(event) {
-  // Basic worker to satisfy PWA requirements
-  event.respondWith(caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-  }));
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/ilocano_dictionary.json',
+  '/manifests.json',
+  '/images/favicon.ico',
+  '/images/android-chrome-192x192.png',
+  '/images/android-chrome-512x512.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        } 
+
+        return fetch(event.request);
+      })
+  );
 });

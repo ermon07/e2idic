1d1460098a11b746b03ddb1072d5e55a12388c20
ermon07 <ermon1995@gmail.com>
2026-04-20 12:47:21 +0800

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -12,24 +12,31 @@ const urlsToCache = [
   '/images/android-chrome-512x512.png',
 ];
 
-self.addEventListener('install', event => {
+sself.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+// FETCH (cache-first strategy)
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        } 
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
 
-        return fetch(event.request);
-      })
+// ACTIVATE (clean old cache)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+      );
+    })
   );
 });
